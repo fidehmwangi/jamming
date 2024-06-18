0fb0bd00b9ed857e0accc6d8b64622718328404e
Fideh Mwangi <fnyaruai@gmail.com>
2024-06-18 04:47:52 +0300

    Created components

---
+++ src/searchResults.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function searchResults () {
+    return (
+
+    )
+}
+
+
+export default searchResults;
---
+++ src/searchbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function searchBar () {
+    return (
+
+    )
+}
+
+export default searchBar;
